2203e672c92c29479a79822e1bad87dee66e66e4
James Barboza <jamesbarboza@Jamess-MacBook-Pro.local>
2025-08-05 00:24:14 +0530

    evaluation of code added

--- tests/fetch_user_policies.spec.ts
+++ tests/fetch_user_policies.spec.ts
@@ -9,18 +9,30 @@ const userAdminConfig = {
 const userForPolicies = 'a1816883-7a80-453f-8eb6-15b2dd91a083';
 
 test('login and fetch user policies', async({ page }) => {
+    await page.route('**', route => route.continue());
     await page.goto(URL);
 
-    const emailInput = await page.locator('input[type=email]').first();
+    const email_input_count = await page.locator('input[type=email]').count();
+    if (email_input_count > 0) {
+        await page.locator('input[type=email]').first().fill(userAdminConfig.email);
+    } else {
+        const fixedCode = `await page.locator('button#popup-skip').first().click(); await page.waitForTimeout(1000);`
+        const runFixedCode = new Function('page', `
+            return (async () => {
+              ${fixedCode}
+            })();
+          `);
+        await runFixedCode(page);
+        // await page.locator('button#popup-skip').first().click();await page.waitForTimeout(1000);
+
+        console.log("pink floyd")
+        await page.locator('input[type=email]').first().fill(userAdminConfig.email);
+    }
+
     const passwordInput = await page.locator('input[type=password]').first();
-    const loginButton = await page.locator('button[type="submit"]').first();
-
-    console.log('emailInput', emailInput);
-    console.log('passwordInput', passwordInput);
-    console.log('loginButton', loginButton);
-
-    await emailInput.fill(userAdminConfig.email);
     await passwordInput.fill(userAdminConfig.password);
+
+    const loginButton = await page.locator('button[type="submit"]').first();
     await loginButton.click();
 
     await page.waitForTimeout(10000);
